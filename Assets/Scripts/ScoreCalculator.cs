ecf242aa5651f9ff087bba3d04f3152ef7def7c7
FionaOwen <968079@swansea.ac.uk>
2023-11-24 20:24:15 +0000

    Updating Golf Court
    Updating the Golf Court. Issue with collider.
    Hand texture updated.

---
+++ Assets/Scripts/ScoreCalculator.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreCalculator : MonoBehaviour
+{
+    public GameObject congratsPanel;
+    public GameObject levelRestriction;
+    [SerializeField]
+    private CapsuleCollider flagPostCollider;
+    public Vector3 prefabSpawnOffsetPosition;
+    private void Start()
+    {
+        flagPostCollider = GetComponent<CapsuleCollider>();
+    }
+    public void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "GolfBall")
+        {
+            Instantiate(congratsPanel, gameObject.transform.position + prefabSpawnOffsetPosition, Quaternion.identity);
+
+            flagPostCollider.enabled = false;
+        }
+    }
+    
+    public void AddScore()
+    {
+
+    }
+}
